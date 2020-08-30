31a7458617d7e64c7d698b5adf1b08f2f673d305
knokko <knokogator@hotmail.com>
2020-08-30 10:09:33 +0200

    Write the unit test for ignoring changes in unused variables

--- src/lib.rs
+++ src/lib.rs
@@ -49,6 +49,10 @@ mod tests {
         fn receive_foo(&mut self, new_foo: u32);
 
         fn receive_bar(&mut self, new_bar: u32);
+
+        fn received_foo(&self) -> bool;
+
+        fn received_bar(&self) -> bool;
     }
 
     impl ExampleStore for Store<ExampleModel> {
@@ -66,6 +70,14 @@ mod tests {
 
         fn receive_bar(&mut self, new_bar: u32) {
             self.receive_change(&PROPERTY_BAR, new_bar);
+        }
+
+        fn received_foo(&self) -> bool {
+            self.received_change(&PROPERTY_FOO)
+        }
+
+        fn received_bar(&self) -> bool {
+            self.received_change(&PROPERTY_BAR)
         }
     }
 
@@ -108,5 +120,66 @@ mod tests {
         assert_eq!(2, count_cell.get());
         // And obviously, the value returned by get_bar should also change
         assert_eq!(8, store.get_bar());
+    }
+
+    #[test]
+    fn test_ignore_unread() {
+
+        let count_cell = Rc::new(Cell::new(0));
+        let ref_count_cell = Rc::clone(&count_cell);
+
+        let mut store = Store::new(
+            ExampleModel { foo: 12, bar: 20 },
+            Box::new(move || { ref_count_cell.set(ref_count_cell.get() + 1)}),
+        );
+
+        // Initially, store shouldn't have received any changes
+        assert!(!store.received_foo());
+
+        // So let's send it a change
+        store.receive_foo(4);
+        // It shouldn't count this one, because we never listered for it
+        assert!(!store.received_foo());
+        assert_eq!(0, count_cell.get());
+
+        // Now we listen for a change
+        store.get_foo();
+        // Now it shouldn't count because the last change was before the last read
+        assert!(!store.received_foo());
+        assert_eq!(0, count_cell.get());
+
+        // Change it again
+        store.receive_foo(1);
+        // Now it's different from what we last read, so it should return true
+        assert!(store.received_foo());
+        assert_eq!(1, count_cell.get());
+        // But received_bar should still return false because we never listened for bar
+        // (and we never received a change for it)
+        assert!(!store.received_bar());
+        // Unless we also listen for and change bar
+        store.get_bar();
+        store.receive_bar(9);
+        assert!(store.received_bar());
+
+        // Forget everything
+        store.forget_tracking_state();
+        // received_foo and received_bar should no longer return true
+        assert!(!store.received_foo());
+        assert!(!store.received_bar());
+        // but received_foo should if we let it receive a change again
+        // and listen for it again
+        store.receive_foo(3);
+        assert!(!store.received_foo());
+        assert_eq!(1, count_cell.get());
+        store.get_foo();
+        store.receive_foo(11);
+        assert!(store.received_foo());
+        assert_eq!(2, count_cell.get());
+        // but received_bar should still return false
+        assert!(!store.received_bar());
+        // even if we receive a change for it, since we didn't listen since we forgot
+        store.receive_bar(0);
+        assert!(!store.received_bar());
+        assert_eq!(2, count_cell.get());
     }
 }
